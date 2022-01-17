44446d9c247390ffb47d09181dc427f08b402458
shra1dhar <shraonedhar@gmail.com>
2022-01-18 02:52:50 +0530

    Meta fav and manifest

--- components/card/card-wrap/index.tsx
+++ components/card/card-wrap/index.tsx
@@ -5,7 +5,7 @@ const CardWrap = () => {
 		<div className="bg-gray-600 flex p-5 mt-8 space-x-4 items-center shadow-xl max-w-sm rounded-md">
 			<img src="https://avatars.githubusercontent.com/u/5550850?v=4" alt="image" className="h-14 w-14 rounded-full" />
 			<div>
-				<h2 className="text-white font-semibold text-xl">bradtraversy</h2>
+				<h2 className="text-white font-semibold text-xl">Shravan Dhar</h2>
 				<p className="mt-1 text-gray-400 text-sm cursor-pointer">Visit Profile</p>
 			</div>
 		</div>
---
+++ components/head/CommonLinks.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const CommonLinks = () => {
+	return (
+		<>
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/site.webmanifest" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6c6c6c" />
+			<meta name="msapplication-TileColor" content="#e1e1e1" />
+			<meta name="theme-color" content="#ffffff" />
+		</>
+	)
+}
